bfa14b79ebe4f0fefa86140307ad63263efb6040
logankarsten <karsten@hydro-c1.rap.ucar.edu>
2016-04-19 17:48:38 +0000

    Update 4/19/2016

--- R/UTILS/PlotBasin.R
+++ R/UTILS/PlotBasin.R
@@ -261,9 +261,9 @@ plotEnsFlowWObs <- function(n, modDfs, obs,
 	spreadDf$mean <- NA
 	spreadDf$ObsCFS <- NA
 
-	print(dfTmp2)
 	for (i in 1:nSteps) {
 		dfTmp2 <- subset(dfTmp, POSIXct == dates[i])
+		print(dfTmp2)
 		qCalc <- quantile(dfTmp2$q_cfs, probs=seq(0,1,0.25), na.rm = TRUE)
 		spreadDf$st_id[i] <- dfTmp2$site_id[1]
 		spreadDf$st_lon[i] <- dfTmp2$st_lon[1]
