84cb2546e1760ae520eecabd536284749d127dcd
logankarsten <karsten@hydro-c1.rap.ucar.edu>
2016-06-14 18:54:04 +0000

    update 6/14/2016

--- R/COMMON/run_CONFIG.R
+++ R/COMMON/run_CONFIG.R
@@ -138,7 +138,6 @@ if (calcStats | createPlots) {
 				load(i)
 				if (exists("obsStrData.map")) obsStrData <- remapData(obsStrData, obsStrData.map)
 				if (exists("obsStrMeta.map")) obsStrMeta <- remapData(obsStrMeta, obsStrMeta.map)
-				print(obsStrData)
 				if ( !is.null(gageList) ) { 
 					obsStrData_TMP <- subset(obsStrData, obsStrData$site_no %in% unique(gageList$site_no))
 					obsStrMeta_TMP <- subset(obsStrMeta, obsStrMeta$site_no %in% unique(gageList$site_no))
