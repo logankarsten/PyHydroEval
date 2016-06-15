a1f9c8356e1324aadf4f2ca1d0cf25202c077b82
logankarsten <karsten@hydro-c1.rap.ucar.edu>
2016-06-15 16:45:16 +0000

    Update 6/15/2016

--- R/UTILS/PlotBasin.R
+++ R/UTILS/PlotBasin.R
@@ -412,7 +412,7 @@ plotEnsFlowWObs <- function(n, modDfs, obs,
 		colOut[length(colOut)] <- 'black'
 		gg <- ggplot(data=dfTmp,aes(x=POSIXct,y=q_cfs,color=enstag)) + geom_line() + 
 	      		geom_line(data=spreadDf, aes(x=POSIXct,y=ObsCFS,color='Observed'),size=1.2,linetype='dashed') + 
-	      		scale_color_manual(name='Model Run',values = colOut,label=c(unique(dfTmp$enstag),'Observed')) + 
+	      		#scale_color_manual(name='Model Run',values = colOut,label=c(unique(dfTmp$enstag),'Observed')) + 
 	      		ggtitle(title) + xlab('Date') + ylab('Streamflow (cfs)') + ylim(0,yMax) + 
 			scale_colour_identity(guide="legend",breaks=colOut)  
         	fileOutPath <- paste0(outDir,'/streamflow_spaghetti_',n,'_',strftime(startDate,"%Y%m%d%H"),
