8a4e962f6bff9d29b8828fa79ec78b7095fa47b1
logankarsten <karsten@hydro-c1.rap.ucar.edu>
2016-08-03 22:42:16 +0000

    Adding baseflow correction

--- R/UTILS/PlotBasin.R
+++ R/UTILS/PlotBasin.R
@@ -372,13 +372,16 @@ plotEnsFlowWObs <- function(n, modDfs, obs,
 			print(dates[i])
 			# Next, calculate minimum ESP forecast value.
 			minCfs <- min(dfTmp2$q_cfs)
-			
+		
+			print(minCfs)	
 			# Next, calculate difference between minimum ESP forecast value and observation from 
 			# the beginning of the forecast period. Apply that value to ALL ESP forecast values.
 			# If modeled streamflow goes below zero, set it to 0. This is for cases where observations
 			# have already gone to zero at the beginning of the forecast time period. 
 			for (j in length(dfTmp2$q_cfs)){
 				diffTmp <- minCfs - obsBaseFlow
+				print(diffTmp)
+				print(j)
 				dfTmp2$q_cfs[j] <- dfTmp2$q_cfs[j] - diffTmp
 				if (dfTmp2$q_cfs[j] < 0.0){
 					dfTmp2$q_cfs[j] <- 0.0
