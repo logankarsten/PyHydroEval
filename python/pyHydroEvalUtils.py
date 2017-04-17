c8348356e1ab07ff4556dc6f325c61e1106220d6
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-04-17 12:52:17 -0600

    update 4/16/17

--- python/pyHydroEvalUtils.py
+++ python/pyHydroEvalUtils.py
@@ -60,7 +60,7 @@ def checkArgs(parser):
 			print "ERROR: Invalid fxRead value."
 			raise
 	if parser.chRead:
-		if (int(parser.chRead) < 1) or (int(parser.chRead) > 2):
+		if (int(parser.chRead) < 1) or (int(parser.chRead) > 3):
 			print "ERROR: Invalid chRead value."
 			raise
 	if parser.forRead:
